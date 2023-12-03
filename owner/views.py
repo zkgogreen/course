33ec242c3b76db773cb5fcd7b8c11e0be396567d
zkgogreen <jakaa354.ja@gmail.com>
2023-12-03 07:33:47 +0700

    add owner base template

--- owner/views.py
+++ owner/views.py
@@ -1,8 +1,9 @@
 from app.app import auth
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     context = {
         
     }
-    return auth(request, 'index', context)
+    return render(request, 'owner/index.html', context)
