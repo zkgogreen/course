54b15eb50fe89540c5658e41205f9887c9774cf6
zkgogreen <jakaa354.ja@gmail.com>
2023-12-03 13:32:37 +0700

    feat : add overall chart

--- owner/views.py
+++ owner/views.py
@@ -2,8 +2,10 @@ from app.app import auth
 from django.shortcuts import render, redirect
 
 # Create your views here.
+context = {
+    'url': 'Home',
+    'title': 'Home',
+    'icon': 'static/assets/icon.png'
+}
 def index(request):
-    context = {
-        
-    }
     return render(request, 'owner/index.html', context)
